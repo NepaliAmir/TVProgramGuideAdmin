4d51e22f0c0aa1ac46720698101f4609d424ed15
NepaliAmir <gcamir15@hotmail.com>
2016-04-05 07:31:40 +0545

    program module edit

--- TVProgramGuideAdmin/Application/Controllers/ProgramsModule.js
+++ TVProgramGuideAdmin/Application/Controllers/ProgramsModule.js
@@ -1,6 +1,18 @@
 ﻿var programs = angular.module('programModule', []);
 programs.controller('programController', ['$scope', '$http', '$filter', "entityService",
 function ($scope, $http, $filter, entityService, ApiCall) {
+    $scope.programDetails = {
+        Id: '',
+        ChannelId: '',
+        ChannelName: '',
+        image: ''
+    }
+
+    entityService.getAllProgramsDetails().then(function (response) {
+        $scope.programs = response.data;
+    })
+    $scope.fnEditProgramDetails = (function () {
+    })
 
 }
 ]);
